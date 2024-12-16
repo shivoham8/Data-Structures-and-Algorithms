76dc207e4816bf817ca06aa7932dcb9aef71800b
Ajay Gupta <ajaygupta230085@gmail.com>
2024-12-16 20:51:25 +0530

    Add files via upload

---
+++ 02_Operators.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 11;
+    double b = 5.56;
+
+    // Result will be of bigger data type
+    cout << "(a/b) = " << a/b << endl;
+
+    cout << (5/(double)2) << endl;
+
+    // Relational Operators (>, <, <=, >=, ==, !=) returns only true(1) or false(0)
+
+    // Logical Operators --> OR(||), AND(&&), NOT(!)
+
+    // BitWise Operators
+
+    return 0;
+}
