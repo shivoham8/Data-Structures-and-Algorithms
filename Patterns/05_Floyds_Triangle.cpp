1cca85c6eaff3fb40e67052867c494b8f69dd16b
Ajay Gupta <ajaygupta230085@gmail.com>
2025-01-15 21:51:59 +0530

    Add files via upload

---
+++ Patterns/05_Floyds_Triangle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, num = 1;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j <= i+1; j++)
+        {
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
