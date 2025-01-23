c671d357b153c1fe0b0f7016dc22f441550ffaaf
Ajay Gupta <ajaygupta230085@gmail.com>
2025-01-24 00:31:31 +0530

    Update 06_Vector.cpp

--- Arrays/06_Vector.cpp
+++ Arrays/06_Vector.cpp
@@ -3,32 +3,36 @@
 
 using namespace std;
 
-int main() {
-    vector<char> ch = {'a','b','c','d','e'};
-    
+int main()
+{
+    vector<char> ch = {'a', 'b', 'c', 'd', 'e'};
+
     cout << "Size: " << ch.size() << endl;
-    for(char val : ch) {
+    for (char val : ch)
+    {
         cout << val << " ";
     }
     cout << endl;
-    
+
     ch.push_back('f');
     cout << "Size: " << ch.size() << endl;
-     for(char val : ch) {
+    for (char val : ch)
+    {
         cout << val << " ";
     }
     cout << endl;
-    
+
     ch.pop_back();
     cout << "Size: " << ch.size() << endl;
-     for(char val : ch) {
+    for (char val : ch)
+    {
         cout << val << " ";
     }
     cout << endl;
-    
+
     cout << ch.front() << endl;
     cout << ch.back() << endl;
-    
+
     cout << ch.at(2) << endl;
 
     return 0;
