078f4245018d8c7a72e388d9c44a47393c976116
Ajay Gupta <ajaygupta230085@gmail.com>
2025-02-11 18:42:24 +0530

    Add files via upload

---
+++ Arrays/14_Bubble_Sort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+// Since the last i elements are already sorted, we do not need to compare them again. Thus, the inner loop only needs to run up to n - i - 1, ignoring the sorted elements at the end.
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {1, 7, 3, 2, 5};
+    int n = sizeof(arr) / sizeof(int);
+
+    bubbleSort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
