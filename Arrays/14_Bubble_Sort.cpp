711b8188d70dd70077c502db03dc5bdaae785d0e
Ajay Gupta <ajaygupta230085@gmail.com>
2025-02-11 18:46:03 +0530

    Update 14_Bubble_Sort.cpp

--- Arrays/14_Bubble_Sort.cpp
+++ Arrays/14_Bubble_Sort.cpp
@@ -5,14 +5,21 @@ void bubbleSort(int arr[], int n)
 {
     for (int i = 0; i < n - 1; i++)
     {
-// Since the last i elements are already sorted, we do not need to compare them again. Thus, the inner loop only needs to run up to n - i - 1, ignoring the sorted elements at the end.
+        bool isSwap = false;
+        // Since the last i elements are already sorted, we do not need to compare them again. Thus, the inner loop only needs to run up to n - i - 1, ignoring the sorted elements at the end.
         for (int j = 0; j < n - i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
                 swap(arr[j], arr[j + 1]);
+                isSwap = true;
             }
         }
+    }
+
+    if (!isSwap)
+    {
+        return;
     }
 }
 
